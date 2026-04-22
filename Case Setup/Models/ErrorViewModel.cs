73c31616afd4daf6e6484d9c7effda4463e6bbee
Holly <holibuksa@gmail.com>
2026-04-22 13:10:51 +0200

    init case setup

---
+++ Case Setup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Case_Setup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
