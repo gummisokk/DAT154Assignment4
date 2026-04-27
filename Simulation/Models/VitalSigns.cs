20661598e6ad90f84ed2001ca25188b4aacba273
h189110 <birk.tangen@outlook.com>
2026-04-27 18:34:59 +0200

    initial val

---
+++ Simulation/Models/VitalSigns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulation.Models
+{
+    internal class VitalSigns
+    {
+        public int HeartRate { get; set; }
+        public int SystolicBP { get; set; }
+        public int DiastolicBP { get; set; }
+        public int RespiratoryRate { get; set; }
+        public int OxygenSaturation { get; set; }
+        public double Temperature { get; set; }
+    }
+}
